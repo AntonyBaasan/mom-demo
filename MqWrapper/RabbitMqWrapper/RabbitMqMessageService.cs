0322348a576a42a618a6ac4651cf0338468eb851
Antony Baasandorj <antony.baasan@gmail.com>
2018-07-20 10:24:50 -0400

    add listener

--- MqWrapper/RabbitMqWrapper/RabbitMqMessageService.cs
+++ MqWrapper/RabbitMqWrapper/RabbitMqMessageService.cs
@@ -4,6 +4,7 @@ using MqService;
 using MqService.Messages;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace RabbitMqService
 {
@@ -19,26 +20,32 @@ namespace RabbitMqService
 
         public void Publish(IMessage message)
         {
-            string channel = "";
-            bool durable = false;
-            bool isBroadcast = false;
-
-            foreach (Attribute attribute in message.GetType().GetCustomAttributes(false))
+            MessageAttribute messageAttribute = GetMessageAttribute(message.GetType());
+            if (messageAttribute == null)
             {
-                MessageAttribute messageAttribute = (MessageAttribute)attribute;
-                channel = messageAttribute.ChannelName;
-                durable = messageAttribute.Durable;
-                isBroadcast = messageAttribute.IsBroadcast;
+                throw new Exception("Can't publish a message with missing MessageAttribute!");
             }
 
-            if (isBroadcast)
+            if (messageAttribute.IsBroadcast)
             {
-                BroadcastToRabbit(channel, durable, message.GetPayload());
+                BroadcastToRabbit(messageAttribute.ChannelName, messageAttribute.Durable, message.GetPayload());
             }
             else
             {
-                QueueToRabbit(channel, durable, message.GetPayload());
+                QueueToRabbit(messageAttribute.ChannelName, messageAttribute.Durable, message.GetPayload());
             }
+        }
+
+        private MessageAttribute GetMessageAttribute(Type messageType)
+        {
+            foreach (Attribute attribute in messageType.GetCustomAttributes(false))
+            {
+                if (attribute is MessageAttribute)
+                {
+                    return (MessageAttribute)attribute;
+                }
+            }
+            return null;
         }
 
         private void BroadcastToRabbit(string queueName, bool durable, object content)
@@ -68,7 +75,45 @@ namespace RabbitMqService
 
         public void ListenMessage<T>(Action callback) where T : IMessage
         {
-            throw new NotImplementedException();
+
+            MessageAttribute messageAttribute = GetMessageAttribute(typeof(T));
+            if (messageAttribute == null)
+            {
+                throw new Exception("Can't listen a message with missing MessageAttribute!");
+            }
+
+            if (messageAttribute.IsBroadcast)
+            {
+            }
+            else
+            {
+                ListenRabbitMessage<T>(messageAttribute.ChannelName, messageAttribute.Durable, callback);
+            }
+        }
+
+        public void ListenRabbitMessage<T>(string channelName, bool durable, Action callback) where T : IMessage
+        {
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: channelName,
+                    durable: durable,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+
+                channel.BasicConsume(queue: channelName,
+                    autoAck: true,
+                    consumer: consumer);
+            }
         }
     }
 }
