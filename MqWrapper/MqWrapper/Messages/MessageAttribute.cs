875749d9b2d026d3b978c4ec9e6639c4bc8e7f73
Antony Baasandorj <antony.baasan@gmail.com>
2018-07-24 12:04:43 -0400

    add comement

--- MqWrapper/MqWrapper/Messages/MessageAttribute.cs
+++ MqWrapper/MqWrapper/Messages/MessageAttribute.cs
@@ -7,6 +7,11 @@ namespace MqWrapper.Messages
     {
         public string ChannelName { get; set; }
         public bool IsBroadcast { get; set; }
+        /// <summary>
+        /// Durable means the message will be saved on the disk, which 
+        /// means the message will restored when MQ server is restarted.
+        /// BUT, this will add more overhead (slower than non durable).
+        /// </summary>
         public bool Durable { get; set; }
     }
 }
